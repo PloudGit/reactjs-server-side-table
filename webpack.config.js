dd44cfe7683d6610fad6f0f9094d98a37b0064f7
mahmutaktas <mahmutaktas@marun.edu.tr>
2022-01-04 11:10:12 +0300

    added new babel and webpack packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,52 @@
+// webpack.config.js
+
+/* eslint-disable */
+const path = require('path')
+const webpack = require('webpack')
+
+const ENVIRONMENT = process.env.NODE_ENV
+const PRODUCTION = ENVIRONMENT === 'production'
+const SOURCEMAP = !PRODUCTION || process.env.SOURCEMAP
+
+const library = 'reactjs-server-side-table'
+const filename = PRODUCTION ? `${library}.min.js` : `${library}.js`
+
+const plugins = []
+
+if (PRODUCTION) {
+  plugins.push(
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(ENVIRONMENT),
+    }),
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      output: { comments: false, semicolons: false },
+      sourceMap: SOURCEMAP,
+    })
+  )
+}
+
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-      filename: "index.js",
-      libraryTarget: 'commonjs2' //This one the most important line, others things will remain same
-    },
-    module: {
-      //Other code
-    }
-  };
+  devtool: SOURCEMAP ? 'source-map' : 'none',
+  entry:  `${__dirname}/src/index.js`,
+  externals: {
+    'react': 'react',
+    'react-dom': 'react-dom',
+  },
+  module: {
+    loaders: [{
+      test:    /\.js$/,
+      loader:  'babel-loader',
+      exclude: /node_modules/,
+    }],
+  },
+  output: {
+    filename,
+    library,
+    path:           `${__dirname}/lib`,
+    libraryTarget:  'umd',
+    umdNamedDefine: true,
+  },
+  plugins,
+}
